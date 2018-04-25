14cc3dbbb5691f948e7b977fe55b846ca0665adb
zhudalun1553 <allen.yl.zhu@gmail.com>
2018-04-24 21:54:53 -0400

    ...

--- code/evaluation.py
+++ code/evaluation.py
@@ -2,10 +2,15 @@ from collections import Counter
 
 class Eval:
     def __init__(self, gold, pred):
-        assert len(gold)==len(pred)
+        # assert len(gold)==len(pred)
         self.gold = gold
         self.pred = pred
 
     def accuracy(self):
+        #baseline
         numer = sum(1 for p,g in zip(self.pred,self.gold) if p==g)
+
+        #bidirectional
+        numer = sum(1 for p,g in zip(self.pred, self.gold) if p in g)
+
         return numer / len(self.gold)
